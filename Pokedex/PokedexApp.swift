4150e35b7e0460fe4d4febdef54c0b0fe56f5086
Zayaan Ali <zayaanali02@gmail.com>
2024-06-10 13:07:04 -0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Zayaan Ali on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
